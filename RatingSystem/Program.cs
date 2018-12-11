6c222f7895ede4e4bf3b3fbf5c9452a712a024b5
saifalgiri@gmail.com <34902733+saifalgiri@users.noreply.github.com>
2018-12-12 00:51:11 +0800

    Add project files.

---
+++ RatingSystem/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ReviewSystem
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var path = System.IO.Directory.GetCurrentDirectory() + "\\HotelReview.json";
+
+            using (StreamReader sr = new StreamReader(path.ToString()))
+            {
+                var json = JObject.Parse(sr.ReadToEnd());
+                var data = json["Data"];
+
+                List<Review> list = JsonConvert.DeserializeObject<List<Review>>(data["Reviews"].ToString());
+
+                list.Where(x => x.Value == 5)
+                    .Select(x => { x.Value = 8; return x; })
+                    .ToList();
+
+                list.Where(y => y.Value == 4)
+                   .Select(y => { y.Value = 6; return y; })
+                   .ToList();
+
+                list.Where(z => z.Value == 2)
+                   .Select(z => { z.Value = -6; return z; })
+                   .ToList();
+
+                list.Where(r => r.Value == 1)
+                   .Select(r => { r.Value = -8; return r; })
+                   .ToList();
+
+                var test = list.GroupBy(c => c.Label)
+                     .Select(c => new
+                     {
+                         Value = c.Sum(y => y.Value),
+                         Label = c.Key,
+                         Hotel = data["Hotel"]
+
+                     })
+                     .ToList();
+
+                foreach (var r in test)
+                {
+                    Console.WriteLine(r.Value >= 0 ? r.Hotel + " is suggested for " + r.Label : r.Hotel + " is not Suggested for " + r.Label);
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ RatingSystem/Review.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ReviewSystem
+{
+    public class Review
+    {
+        [JsonProperty(PropertyName = "Value")]
+        public int Value { get; set; }
+
+        [JsonProperty(PropertyName = "Room")]
+        public int Room { get; set; }
+
+        [JsonProperty(PropertyName = "Hotel")]
+        public string HotelName { get; set; }
+
+        [JsonProperty(PropertyName = "Title")]
+        public string Title { get; set; }
+
+        [JsonProperty(PropertyName = "Content")]
+        public string Label { get; set; }
+
+        [JsonProperty(PropertyName = "Date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty(PropertyName = "Author")]
+        public string Author { get; set; }
+    }
+}
